900144a4222dc5b73893eced6073b5a9db19a6a1
monicamarshall <61441459+monicamarshall@users.noreply.github.com>
2021-06-01 21:52:20 -0600

    Add files via upload
    first commit

---
+++ students/authentication.py
@@ -0,0 +1,68 @@
+import jwt
+from rest_framework.authentication import BaseAuthentication
+from django.middleware.csrf import CsrfViewMiddleware
+from rest_framework import exceptions
+from django.conf import settings
+from django.contrib.auth import get_user_model
+from rest_framework_simplejwt.authentication import JWTAuthentication
+
+import logging
+logger = logging.getLogger(__name__)
+
+class CSRFCheck(CsrfViewMiddleware):
+    def _reject(self, request, reason):
+        # Return the failure reason instead of an HttpResponse
+        return reason
+
+
+class SafeJWTAuthentication(JWTAuthentication):
+    '''
+        custom authentication class for DRF and JWT
+        https://github.com/encode/django-rest-framework/blob/master/rest_framework/authentication.py
+    '''
+
+    def authenticate(self, request):
+        logger.debug("In authenicate")
+        User = get_user_model()
+        authorization_heaader = request.headers.get('Authorization')
+        logger.debug("authorization header = " + str(authorization_heaader))
+
+        if not authorization_heaader:
+            return None
+        try:
+            # header = 'Token xxxxxxxxxxxxxxxxxxxxxxxx'
+            access_token = authorization_heaader.split(' ')[1]
+            logger.debug("access_token : " + access_token)
+            #payload = jwt.decode(
+                #access_token, settings.SECRET_KEY, algorithms=['HS256'])
+            public_key = open('publicKey.pem').read()
+            payload = jwt.decode(access_token, public_key, algorithms=['RS256'])
+
+        except jwt.ExpiredSignatureError:
+            raise exceptions.AuthenticationFailed('access_token expired')
+        except IndexError:
+            raise exceptions.AuthenticationFailed('Token prefix missing')
+
+        user = User.objects.filter(id=payload['user_id']).first()
+        if user is None:
+            raise exceptions.AuthenticationFailed('User not found')
+
+        if not user.is_active:
+            raise exceptions.AuthenticationFailed('user is inactive')
+
+        self.enforce_csrf(request)
+        return (user, None)
+
+    def enforce_csrf(self, request):
+        """
+        Enforce CSRF validation
+        """
+        check = CSRFCheck()
+        # populates request.META['CSRF_COOKIE'], which is used in process_view()
+        check.process_request(request)
+        reason = check.process_view(request, None, (), {})
+        print(reason)
+        if reason:
+            # CSRF failed, bail with explicit error message
+            raise exceptions.PermissionDenied('CSRF Failed: %s' % reason)
+
